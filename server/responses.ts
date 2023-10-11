cb41669ea861483694a8df38d4de42788fb109ec
Amirabbas Kazeminia <amirka@mit.edu>
2023-10-11 18:59:06 -0400

    Made the resposes more human readible

--- server/responses.ts
+++ server/responses.ts
@@ -1,8 +1,9 @@
-import { User } from "./app";
+import { ObjectId } from "mongodb";
+import { Post, User } from "./app";
+import { CommentDoc } from "./concepts/comment";
 import { AlreadyFriendsError, FriendNotFoundError, FriendRequestAlreadyExistsError, FriendRequestDoc, FriendRequestNotFoundError } from "./concepts/friend";
 import { PostAuthorNotMatchError, PostDoc } from "./concepts/post";
 import { Router } from "./framework/router";
-
 /**
  * This class does useful conversions for the frontend.
  * For example, it converts a {@link PostDoc} into a more readable format for the frontend.
@@ -25,6 +26,27 @@ export default class Responses {
   static async posts(posts: PostDoc[]) {
     const authors = await User.idsToUsernames(posts.map((post) => post.author));
     return posts.map((post, i) => ({ ...post, author: authors[i] }));
+  }
+
+  /**
+   * Convert CommentDoc into more readable format for the frontend by converting the author id into a username and a post if to a content.
+   */
+  static async comment(comment: CommentDoc | null) {
+    if (!comment) {
+      return comment;
+    }
+    const author = await User.getUserById(comment.author);
+    const target = await Post.getPostById(comment.target);
+    return { ...comment, author: author.username, target: target.content };
+  }
+
+  /**
+   * Same as {@link comment} but for an array of CommentDoc for improved performance.
+   */
+  static async comments(comments: CommentDoc[]) {
+    const authors = await User.idsToUsernames(comments.map((comment) => comment.author));
+    const targets = await Post.idsToPost(comments.map((comment) => new ObjectId(comment.target)));
+    return comments.map((comment, i) => ({ ...comment, author: authors[i], target: targets[i] }));
   }
 
   /**
